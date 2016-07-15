fff3d1401fe7462da671f6e58c7cec9db808a7e0
Guy Golan <guygolan91@gmail.com>
2016-07-15 16:26:02 +0300

    fixed direct to old home after login\off

--- src/ConferenceProject/Controllers/AccountController.cs
+++ src/ConferenceProject/Controllers/AccountController.cs
@@ -118,7 +118,7 @@ namespace ConferenceProject.Controllers
                     //    "Please confirm your account by clicking this link: <a href=\"" + callbackUrl + "\">link</a>");
                     await _signInManager.SignInAsync(user, isPersistent: false);
                     _logger.LogInformation(3, "User created a new account with password.");
-                    return RedirectToAction(nameof(HomeController.Index), "Home");
+                    return RedirectToAction(nameof(ConferenceItemsController.Home), "ConferenceItems");
                 }
                 AddErrors(result);
             }
@@ -135,7 +135,7 @@ namespace ConferenceProject.Controllers
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation(4, "User logged out.");
-            return RedirectToAction(nameof(HomeController.Index), "Home");
+            return RedirectToAction(nameof(ConferenceItemsController.Home), "ConferenceItems");
         }
 
         //
@@ -463,7 +463,7 @@ namespace ConferenceProject.Controllers
             }
             else
             {
-                return RedirectToAction(nameof(HomeController.Index), "Home");
+                return RedirectToAction(nameof(ConferenceItemsController.Home), "ConferenceItems");
             }
         }
 
