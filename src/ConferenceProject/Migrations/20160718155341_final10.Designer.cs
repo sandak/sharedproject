b448f6bfd7f919d356cde11c4e344a194a341f0a
amit sandak <sandak.amit@gmail.com>
2016-07-18 18:54:34 +0300

    final fix!!

--- src/ConferenceProject/Migrations/20160718155341_final10.Designer.cs
+++ src/ConferenceProject/Migrations/20160718155341_final10.Designer.cs
@@ -8,9 +8,10 @@ using ConferenceProject.Models;
 namespace ConferenceProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160718155341_final10")]
+    partial class final10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
